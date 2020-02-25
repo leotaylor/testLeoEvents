84fa8436b23e9d1b50fa424a5ea46f4a39787017
Leo Taylor <leotaylor723@gmail.com>
2020-02-25 12:13:36 -0600

    Testing SourceTree with small change

--- src/components/functionalButton/testButton.js
+++ src/components/functionalButton/testButton.js
@@ -5,7 +5,7 @@ export default function TestButton(props) {
     return (
         <div>
         {!toggle ? 
-            <button className="btn btn-danger" onClick={(clickedIt)}>Don't Press The Button</button>
+            <button className="btn btn-danger" onClick={(clickedIt)}>Don't Press The Button!!!</button>
             :
             <button className="btn btn-success" onClick={clickedIt}>YOU BROKE IT!!!!!!</button>
         }
